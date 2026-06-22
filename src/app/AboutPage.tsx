871e2602190bb0c78d71f16a170426d35961b5d5
Melina Chinni <melinabelenchinni@gmail.com>
2026-06-22 13:01:24 -0300

    Nosotros: pagina reducida a 5 secciones + WhoWeAre mas conciso (sin pilares largos)

--- src/app/AboutPage.tsx
+++ src/app/AboutPage.tsx
@@ -2,13 +2,8 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { AboutHero } from './components/about/AboutHero';
 import { WhoWeAre } from './components/about/WhoWeAre';
-import { MisionVision } from './components/about/MisionVision';
 import { Valores } from './components/about/Valores';
-import { CircularEconomy } from './components/about/CircularEconomy';
-import { ImpactoKPIs } from './components/about/ImpactoKPIs';
 import { TeamSection } from './components/about/TeamSection';
-import { ComoTrabajamos } from './components/about/ComoTrabajamos';
-import { ClientesAlianzas } from './components/about/ClientesAlianzas';
 import { FinalCTA } from './components/about/FinalCTA';
 
 export default function AboutPage() {
@@ -18,13 +13,8 @@ export default function AboutPage() {
       <main>
         <AboutHero />
         <WhoWeAre />
-        <MisionVision />
         <Valores />
-        <CircularEconomy />
-        <ImpactoKPIs />
         <TeamSection />
-        <ComoTrabajamos />
-        <ClientesAlianzas />
         <FinalCTA />
       </main>
       <Footer />
