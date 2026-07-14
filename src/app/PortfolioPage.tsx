b7a3680cd8013b13f1bb2da051a488aaea00e19a
Melina Chinni <melinabelenchinni@gmail.com>
2026-07-14 11:26:34 -0300

    Simplificacion: subpaginas mas limpias (servicios 9->5, portfolio 10->6, membresia 10->6, contacto 7->4)

--- src/app/PortfolioPage.tsx
+++ src/app/PortfolioPage.tsx
@@ -4,11 +4,7 @@ import { PortfolioHero } from './components/portfolio/PortfolioHero';
 import { ClientLogoWall } from './components/portfolio/ClientLogoWall';
 import { ProjectsGallery } from './components/portfolio/ProjectsGallery';
 import { FeaturedCase } from './components/portfolio/FeaturedCase';
-import { CaseProducts } from './components/portfolio/CaseProducts';
-import { CaseServices } from './components/portfolio/CaseServices';
 import { ResultsMetrics } from './components/portfolio/ResultsMetrics';
-import { PortfolioTestimonials } from './components/portfolio/Testimonials';
-import { MoreProjects } from './components/portfolio/MoreProjects';
 import { PortfolioFinalCTA } from './components/portfolio/PortfolioFinalCTA';
 
 export default function PortfolioPage() {
@@ -20,11 +16,7 @@ export default function PortfolioPage() {
         <ClientLogoWall />
         <ProjectsGallery />
         <FeaturedCase />
-        <CaseProducts />
-        <CaseServices />
         <ResultsMetrics />
-        <PortfolioTestimonials />
-        <MoreProjects />
         <PortfolioFinalCTA />
       </main>
       <Footer />
